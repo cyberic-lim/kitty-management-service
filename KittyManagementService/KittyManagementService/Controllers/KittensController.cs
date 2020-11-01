5d125f798bbc1fe56c6fe2546d4721c18f740fe1
eric_lim <eric_lim@dev.net>
2020-11-01 10:00:05 +0800

    initial commit

---
+++ KittyManagementService/KittyManagementService/Controllers/KittensController.cs
@@ -0,0 +1,22 @@
+﻿using KittyManagementService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KittyManagementService.Controllers
+{
+    [ApiController]
+    [Route("api/v1/kittens")]
+    public class KittensController : ControllerBase
+    {
+        [HttpGet()]
+        public KittenDto GetKittens()
+        {
+            KittenDto kittenDto = new KittenDto { KittenId = 1, Name = "Mimi" };
+
+            return kittenDto;
+        }
+    }
+}
---
+++ KittyManagementService/KittyManagementService/Models/KittenDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KittyManagementService.Models
+{
+    public class KittenDto
+    {
+        public int KittenId { get; set; }
+        public string Name { get; set; }
+    }
+}
