4654825e77f99e43b26f9cea16fa8042d7cd4267
eric_lim <eric_lim@dev.net>
2021-01-12 17:32:18 +0800

    added NUnit testing

---
+++ KittyManagementService/KittyManagementServiceTest/TestMethods/KittensControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using KittyManagementService.Controllers;
+using KittyManagementService.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KittyManagementServiceTest.TestMethods
+{
+    class KittensControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestGetKittens()
+        {
+            KittensController kittensController = new KittensController();
+            KittenDto kittenDto = kittensController.GetKittens();
+            Assert.IsTrue(kittenDto.Name == "Mango");
+        }
+
+    }
+}
